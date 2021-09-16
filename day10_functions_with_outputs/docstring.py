b569c8a9cab8f628436d358a042918c56076c181
juan camacho <juanc@tiv.solutions>
2021-09-16 00:18:16 -0400

    day 10

---
+++ day10_functions_with_outputs/docstring.py
@@ -0,0 +1,6 @@
+def sum(a,b):
+  """adds 2 numbers"""
+  return a + b
+
+# When calling the function, the pop up in vscode will show the description
+# of what the code does 
---
+++ day10_functions_with_outputs/jsdoc.js
@@ -0,0 +1,11 @@
+/**
+ * Foo takes any argument.
+ * The return value is 'baz' in all cases.
+ * @param {*} bar - Any argument
+ * @param {string} [optionalArg] - An optional argument that is a string
+ */
+ function foo(bar, optionalArg) {
+  return 'baz';
+}
+
+foo
