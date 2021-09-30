63df57fe97d7ef8f5a13a2b878448e0e7ecd8dbf
juan camacho <juanc@tiv.solutions>
2021-09-29 23:19:41 -0400

    day 23 turtle corssing

---
+++ 21-30/day23_turtle_crossing/main.py
@@ -0,0 +1,42 @@
+import time
+from turtle import Screen
+from typing import ClassVar
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+y_positions = [-70, -40, -10, 20, 50, 80]
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "w")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_car()
+
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
