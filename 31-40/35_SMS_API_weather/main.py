9231ae5ad99408ab3f9a3d7f3392fed440e1f6ad
juan camacho <juanc@tiv.solutions>
2021-10-27 20:08:50 -0400

    day 35,36

---
+++ 31-40/35_SMS_API_weather/main.py
@@ -0,0 +1,27 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = 'fd4262e97d395dae4da9895a5f543573'
+
+
+weather_params = {
+    "lat": 40.725098,
+    "lon": -73.245392,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
